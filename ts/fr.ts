fcd72c77836d9201b4708dc1899771db8a78e5fe
ebeuque <ebeuque@7b800444-9e05-4732-b7a2-3a34cb220e87>
2016-06-16 10:34:37 +0000

    Internationalize application

--- ts/fr.ts
+++ ts/fr.ts
@@ -2,6 +2,71 @@
 <!DOCTYPE TS>
 <TS version="2.1">
 <context>
+    <name>Main</name>
+    <message>
+        <location filename="../src/main.cpp" line="81"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="81"/>
+        <source>Error to initialize application</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QDatabaseConnectionViewController</name>
+    <message>
+        <location filename="../src/GUIController/QDatabaseConnectionViewController.cpp" line="45"/>
+        <source>worksheet</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QDatabaseSelectionView</name>
+    <message>
+        <location filename="../src/GUI/QDatabaseSelectionView.cpp" line="70"/>
+        <source>(None)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QDatabaseSelectionViewController</name>
+    <message>
+        <location filename="../src/GUIController/QDatabaseSelectionViewController.cpp" line="62"/>
+        <source>No file selected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/GUIController/QDatabaseSelectionViewController.cpp" line="62"/>
+        <source>Please choose a file to open.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/GUIController/QDatabaseSelectionViewController.cpp" line="67"/>
+        <source>new tab</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QDatabaseTableView</name>
+    <message>
+        <location filename="../src/GUI/QDatabaseTableView.cpp" line="58"/>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/GUI/QDatabaseTableView.cpp" line="62"/>
+        <source>Filter:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/GUI/QDatabaseTableView.cpp" line="68"/>
+        <source>Clear</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>QWindowMain</name>
     <message>
         <location filename="../src/GUI/QWindowMain.cpp" line="67"/>
@@ -56,6 +121,19 @@
     <message>
         <location filename="../src/GUI/QWindowMain.cpp" line="88"/>
         <source>&amp;About</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QWindowMainController</name>
+    <message>
+        <location filename="../src/GUIController/QWindowMainController.cpp" line="67"/>
+        <source>About...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/GUIController/QWindowMainController.cpp" line="67"/>
+        <source>Text</source>
         <translation type="unfinished"></translation>
     </message>
 </context>

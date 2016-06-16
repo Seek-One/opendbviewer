e75e5c0a57436cf25d821857dcc5a099d1fc9845
ebeuque <ebeuque@7b800444-9e05-4732-b7a2-3a34cb220e87>
2016-06-16 10:30:48 +0000

    Internationalize application

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,18 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+#ifdef HAVE_VERSION_H
+#include <version.h>
+#endif
+
+#include <QApplication>
+#include <QTranslator>
+#include <QLibraryInfo>
+#include <QMessageBox>
+
+#include "GUI/QWindowMain.h"
+#include "GUIController/QWindowMainController.h"
+
 // Enable memory leak detection
 #if defined(_MSC_VER) && defined(_DEBUG)
 #define _CRTDBG_MAP_ALLOC
@@ -8,27 +23,72 @@
 #define new DEBUG_NEW
 #endif // _DEBUG
 
-#include <QApplication>
-#include <QPushButton>
-
-#include "GUI/QWindowMain.h"
-
-#include "GUIController/QWindowMainController.h"
-
 int main(int argc, char *argv[])
 {
 #if defined(_MSC_VER) && defined(_DEBUG)
 	_CrtSetDbgFlag ( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
 #endif
+	bool bGoOn = true;
 
-    QApplication a( argc, argv );
+	// Initialize the QT application
+	QApplication app(argc, argv);
+	QApplication::setApplicationName(APPLICATION_NAME);
+	QApplication::setApplicationVersion(APPLICATION_VERSION);
+	QApplication::setOrganizationName(APPLICATION_VENDOR_NAME);
+	QApplication::setOrganizationDomain(APPLICATION_VENDOR_DOMAIN);
+	//QApplication::setWindowIcon(QIcon(":/"APPLICATION_PACKAGE_NAME".png"));
 
-    QWindowMain window;
+	qDebug("[Main] Starting application");
 
-    QWindowMainController controller;
-    controller.init(&window);
+	// Initialize translation
+	QTranslator qtTranslator;
+	qDebug("[Main] Current locale is %s", qPrintable(QLocale::system().name()));
+	// qDebug("[Main] Current locale path is %s", qPrintable(QLibraryInfo::location(QLibraryInfo::TranslationsPath)));
+	qtTranslator.load("qt_" + QLocale::system().name(), QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+	app.installTranslator(&qtTranslator);
+	QTranslator appTranslator;
+	appTranslator.load(QLocale::system().name(), ":/ts/");
+	app.installTranslator(&appTranslator);
 
-    window.show();
+	// Init GUI
+	QWindowMain* pWindowMain = NULL;
+	QWindowMainController controller;
+	if(bGoOn){
+		qDebug("[Main] Initializing GUI");
+		pWindowMain = new QWindowMain();
+		controller.init(pWindowMain);
+	}
 
-    return a.exec();
+	// Show GUI
+	if(bGoOn && pWindowMain){
+		qDebug("[Main] Show application");
+		pWindowMain->show();
+		/*
+#ifdef WIN32
+		pWindowMain->setWindowIcon(QIcon(":/"APPLICATION_PACKAGE_NAME".png"));
+#else
+		pWindowMain->setWindowIconText(APPLICATION_PACKAGE_NAME);
+#endif
+		 */
+	}
+
+	// Run the event loop
+	if(bGoOn && pWindowMain){
+		qDebug("[Main] Starting the main loop");
+		app.exec();
+		qDebug("[Main] Exiting the main loop");
+	}else{
+		QMessageBox msgBox(QMessageBox::Critical, QT_TRANSLATE_NOOP("Main", "Error"), QT_TRANSLATE_NOOP("Main", "Error to initialize application"), QMessageBox::Ok);
+		msgBox.exec();
+	}
+
+	// Delete the UI
+	if(pWindowMain){
+		delete pWindowMain;
+		pWindowMain = NULL;
+	}
+
+	qDebug("[Main] Shutdown application");
+
+	return 0;
 }
---
+++ ts/fr.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>QWindowMain</name>
+    <message>
+        <location filename="../src/GUI/QWindowMain.cpp" line="67"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/GUI/QWindowMain.cpp" line="68"/>
+        <source>&amp;New connection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/GUI/QWindowMain.cpp" line="71"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/GUI/QWindowMain.cpp" line="75"/>
+        <source>&amp;Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/GUI/QWindowMain.cpp" line="76"/>
+        <source>&amp;Cut</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/GUI/QWindowMain.cpp" line="78"/>
+        <source>&amp;Copy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/GUI/QWindowMain.cpp" line="80"/>
+        <source>&amp;Paste</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/GUI/QWindowMain.cpp" line="82"/>
+        <source>&amp;Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/GUI/QWindowMain.cpp" line="85"/>
+        <source>&amp;View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/GUI/QWindowMain.cpp" line="87"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/GUI/QWindowMain.cpp" line="88"/>
+        <source>&amp;About</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>

8fbc494974ca724191301529e04267147d6449fe
Eric Beuque <eric.beuque@jet1oeil.com>
2017-09-11 13:06:36 +0100

    Update french translation

--- ts/fr.ts
+++ ts/fr.ts
@@ -11,7 +11,7 @@
     <message>
         <location filename="../src/main.cpp" line="80"/>
         <source>Error to initialize application</source>
-        <translation type="unfinished"></translation>
+        <translation>Erreur à l&apos;initialisation de l&apos;application</translation>
     </message>
 </context>
 <context>
@@ -19,32 +19,32 @@
     <message>
         <location filename="../src/GUI/QAboutDialog.cpp" line="45"/>
         <source>This program is distributed under the terms of the &lt;a href=&quot;https://www.gnu.org/licenses/gpl-3.0.fr.html&quot;&gt;GNU GPL version 3&lt;/a&gt;.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ce programme est distribué sous les termes de la license &lt;a href=&quot;https://www.gnu.org/licenses/gpl-3.0.fr.html&quot;&gt;GNU GPL version 3&lt;/a&gt;.</translation>
     </message>
     <message>
         <location filename="../src/GUI/QAboutDialog.cpp" line="46"/>
         <source>%1 is a product originally created by Eric Beuque in Python.</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 est un produit originallement créé par Eric Beuque en Python.</translation>
     </message>
     <message>
         <location filename="../src/GUI/QAboutDialog.cpp" line="47"/>
         <source>It has been rewritten in C++ and sponsored by %3: &lt;a href=&quot;http://%4&quot;&gt;%4&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Il a été ré-écrit en C++ et sponsorisé par %3 : &lt;a href=&quot;http://%4&quot;&gt;%4&lt;/a&gt;</translation>
     </message>
     <message>
         <location filename="../src/GUI/QAboutDialog.cpp" line="49"/>
         <source>This product uses open source softwares:</source>
-        <translation type="unfinished"></translation>
+        <translation>Ce produit utilise des logiciels open source :</translation>
     </message>
     <message>
         <location filename="../src/GUI/QAboutDialog.cpp" line="80"/>
         <source>About %1</source>
-        <translation type="unfinished"></translation>
+        <translation>A propos %1</translation>
     </message>
     <message>
         <location filename="../src/GUI/QAboutDialog.cpp" line="108"/>
         <source>About Qt</source>
-        <translation type="unfinished"></translation>
+        <translation>A propos de Qt</translation>
     </message>
 </context>
 <context>
@@ -52,12 +52,32 @@
     <message>
         <location filename="../src/GUI/QDatabaseConnectionView.cpp" line="80"/>
         <source>Refresh</source>
-        <translation type="unfinished"></translation>
+        <translation>Rafraîchir</translation>
     </message>
     <message>
         <location filename="../src/GUI/QDatabaseConnectionView.cpp" line="83"/>
         <source>New</source>
-        <translation type="unfinished"></translation>
+        <translation>Nouveau</translation>
+    </message>
+    <message>
+        <location filename="../src/GUI/QDatabaseConnectionView.cpp" line="92"/>
+        <source>Structure</source>
+        <translation>Structure</translation>
+    </message>
+    <message>
+        <location filename="../src/GUI/QDatabaseConnectionView.cpp" line="97"/>
+        <source>Tables</source>
+        <translation>Tables</translation>
+    </message>
+    <message>
+        <location filename="../src/GUI/QDatabaseConnectionView.cpp" line="102"/>
+        <source>System tables</source>
+        <translation>Tables système</translation>
+    </message>
+    <message>
+        <location filename="../src/GUI/QDatabaseConnectionView.cpp" line="107"/>
+        <source>Views</source>
+        <translation>Vues</translation>
     </message>
 </context>
 <context>
@@ -65,23 +85,23 @@
     <message>
         <location filename="../src/GUIController/QDatabaseConnectionViewController.cpp" line="49"/>
         <source>worksheet</source>
-        <translation type="unfinished"></translation>
+        <translation>feuille de travail</translation>
     </message>
     <message>
         <location filename="../src/GUIController/QDatabaseConnectionViewController.cpp" line="65"/>
         <location filename="../src/GUIController/QDatabaseConnectionViewController.cpp" line="87"/>
         <source>Database problem</source>
-        <translation type="unfinished"></translation>
+        <translation>Problème de base de donnée</translation>
     </message>
     <message>
         <location filename="../src/GUIController/QDatabaseConnectionViewController.cpp" line="65"/>
         <source>Problem while loading database tables</source>
-        <translation type="unfinished"></translation>
+        <translation>Problème durant le chargement des tables de la base de données</translation>
     </message>
     <message>
         <location filename="../src/GUIController/QDatabaseConnectionViewController.cpp" line="87"/>
         <source>Problem while loading database table informations</source>
-        <translation type="unfinished"></translation>
+        <translation>Problème durant le chargement des informations de la table</translation>
     </message>
 </context>
 <context>
@@ -89,32 +109,32 @@
     <message>
         <location filename="../src/GUI/QDatabaseTableView.cpp" line="26"/>
         <source>Structure</source>
-        <translation type="unfinished"></translation>
+        <translation>Structure</translation>
     </message>
     <message>
         <location filename="../src/GUI/QDatabaseTableView.cpp" line="31"/>
         <source>Data</source>
-        <translation type="unfinished"></translation>
+        <translation>Données</translation>
     </message>
     <message>
         <location filename="../src/GUI/QDatabaseTableView.cpp" line="36"/>
         <source>Original SQL creation script</source>
-        <translation type="unfinished"></translation>
+        <translation>Script SQL original de création</translation>
     </message>
     <message>
         <location filename="../src/GUI/QDatabaseTableView.cpp" line="74"/>
         <source>Filter:</source>
-        <translation type="unfinished"></translation>
+        <translation>Filtrer :</translation>
     </message>
     <message>
         <location filename="../src/GUI/QDatabaseTableView.cpp" line="80"/>
         <source>Execute</source>
-        <translation type="unfinished"></translation>
+        <translation>Exécuter</translation>
     </message>
     <message>
         <location filename="../src/GUI/QDatabaseTableView.cpp" line="83"/>
         <source>Clear</source>
-        <translation type="unfinished"></translation>
+        <translation>Effacer</translation>
     </message>
 </context>
 <context>
@@ -122,63 +142,63 @@
     <message>
         <location filename="../src/GUI/QOpenDatabaseDialog.cpp" line="37"/>
         <source>SQLite</source>
-        <translation type="unfinished"></translation>
+        <translation>SQLite</translation>
     </message>
     <message>
         <location filename="../src/GUI/QOpenDatabaseDialog.cpp" line="41"/>
         <source>MySQL</source>
-        <translation type="unfinished"></translation>
+        <translation>MySQL</translation>
     </message>
     <message>
         <location filename="../src/GUI/QOpenDatabaseDialog.cpp" line="47"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Annuler</translation>
     </message>
     <message>
         <location filename="../src/GUI/QOpenDatabaseDialog.cpp" line="49"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>OK</translation>
     </message>
     <message>
         <location filename="../src/GUI/QOpenDatabaseDialog.cpp" line="66"/>
         <location filename="../src/GUI/QOpenDatabaseDialog.cpp" line="97"/>
         <source>Connection:</source>
-        <translation type="unfinished"></translation>
+        <translation>Connexion :</translation>
     </message>
     <message>
         <location filename="../src/GUI/QOpenDatabaseDialog.cpp" line="82"/>
         <source>Browse</source>
-        <translation type="unfinished"></translation>
+        <translation>Parcourir</translation>
     </message>
     <message>
         <location filename="../src/GUI/QOpenDatabaseDialog.cpp" line="85"/>
         <source>File:</source>
-        <translation type="unfinished"></translation>
+        <translation>Fichier :</translation>
     </message>
     <message>
         <location filename="../src/GUI/QOpenDatabaseDialog.cpp" line="106"/>
         <source>Host:</source>
-        <translation type="unfinished"></translation>
+        <translation>Hôte :</translation>
     </message>
     <message>
         <location filename="../src/GUI/QOpenDatabaseDialog.cpp" line="113"/>
         <source>Port:</source>
-        <translation type="unfinished"></translation>
+        <translation>Port :</translation>
     </message>
     <message>
         <location filename="../src/GUI/QOpenDatabaseDialog.cpp" line="118"/>
         <source>Username:</source>
-        <translation type="unfinished"></translation>
+        <translation>Nom d&apos;utilisateur</translation>
     </message>
     <message>
         <location filename="../src/GUI/QOpenDatabaseDialog.cpp" line="124"/>
         <source>Password:</source>
-        <translation type="unfinished"></translation>
+        <translation>Mot de passe :</translation>
     </message>
     <message>
         <location filename="../src/GUI/QOpenDatabaseDialog.cpp" line="129"/>
         <source>Database:</source>
-        <translation type="unfinished"></translation>
+        <translation>Base de donnée :</translation>
     </message>
 </context>
 <context>
@@ -186,42 +206,42 @@
     <message>
         <location filename="../src/GUIController/QOpenDatabaseDialogController.cpp" line="44"/>
         <source>Select a file</source>
-        <translation type="unfinished"></translation>
+        <translation>Sélectionner un fichier</translation>
     </message>
     <message>
         <location filename="../src/GUIController/QOpenDatabaseDialogController.cpp" line="44"/>
         <source>SQLite files (*.sqlite *.db)</source>
-        <translation type="unfinished"></translation>
+        <translation>Fichier SQLite (*.sqlite *.db)</translation>
     </message>
     <message>
         <location filename="../src/GUIController/QOpenDatabaseDialogController.cpp" line="76"/>
         <source>Please select a valid SQLite file</source>
-        <translation type="unfinished"></translation>
+        <translation>Veuillez choisir un fichier SQLite valide</translation>
     </message>
     <message>
         <location filename="../src/GUIController/QOpenDatabaseDialogController.cpp" line="99"/>
         <source>Unable to connect to the database, please check the connection information.</source>
-        <translation type="unfinished"></translation>
+        <translation>Impossible de se connecter à la base de donnée, veuillez vérifier les informations de connexion.</translation>
     </message>
     <message>
         <location filename="../src/GUIController/QOpenDatabaseDialogController.cpp" line="114"/>
         <source>Database problem</source>
-        <translation type="unfinished"></translation>
+        <translation>Problème de base de donnée</translation>
     </message>
     <message>
         <location filename="../src/GUIController/QOpenDatabaseDialogController.cpp" line="114"/>
         <source>Problem while loading database tables</source>
-        <translation type="unfinished"></translation>
+        <translation>Problème au chargement des tables de la base de données</translation>
     </message>
     <message>
         <location filename="../src/GUIController/QOpenDatabaseDialogController.cpp" line="127"/>
         <source>Connection error</source>
-        <translation type="unfinished"></translation>
+        <translation>Erreur de connexion</translation>
     </message>
     <message>
         <location filename="../src/GUIController/QOpenDatabaseDialogController.cpp" line="86"/>
         <source>Please enter the necessary information.</source>
-        <translation type="unfinished"></translation>
+        <translation>Veuillez entrez les informations nécessaires.</translation>
     </message>
 </context>
 <context>
@@ -229,27 +249,27 @@
     <message>
         <location filename="../src/GUI/QWindowMain.cpp" line="50"/>
         <source>&amp;File</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Fichier</translation>
     </message>
     <message>
         <location filename="../src/GUI/QWindowMain.cpp" line="51"/>
         <source>&amp;New connection</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Nouvelle connexion</translation>
     </message>
     <message>
         <location filename="../src/GUI/QWindowMain.cpp" line="54"/>
         <source>&amp;Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Quitter</translation>
     </message>
     <message>
         <location filename="../src/GUI/QWindowMain.cpp" line="72"/>
         <source>&amp;Help</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Aide</translation>
     </message>
     <message>
         <location filename="../src/GUI/QWindowMain.cpp" line="73"/>
         <source>&amp;About</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;A propos</translation>
     </message>
 </context>
 </TS>
